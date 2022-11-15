ef00f4f4a6dfccfac54acd4217477947a49e219d
김민재 <minjae2246@ubcare.co.kr>
2022-11-15 18:00:23 +0900

    preview 저장위치 리스트 작업 중

--- src/Components/Preview/Preview.tsx
+++ src/Components/Preview/Preview.tsx
@@ -28,12 +28,21 @@ import LockIcon from "@mui/icons-material/Lock";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import { WriteExitButton, WriteRegisterButton } from "Pages/Write/Write.styles";
 import { GlobalStyle } from "Styles/global.styles";
-import { media } from "Styles/media.styles";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
 
 export default function Preview(props: any) {
     const [intro, setIntro] = useState("");
     const [path, setPath] = useState("");
     const [isOpen, setOpen] = useState(true);
+    const [isSave, setSave] = useState(true);
+    const [selectedIndex, setSelectedIndex] = React.useState(1);
 
     const my = {
         src: "https://velog.velcdn.com/images/minj9_6/profile/f8889f8f-fa44-4ef3-984c-616c55410ad5/P20200203_225352227_66B88E27-7A21-4EC5-987F-0B0457A4AA0C.jpeg",
@@ -50,6 +59,10 @@ export default function Preview(props: any) {
         }
     };
 
+    const clickSave = (e: React.MouseEvent<HTMLButtonElement>): void => {
+        setSave(!isSave);
+    };
+
     const clickOpen = (
         e: React.MouseEvent<HTMLButtonElement>,
         isOpen: boolean
@@ -57,9 +70,12 @@ export default function Preview(props: any) {
         setOpen(isOpen);
     };
 
-    useEffect(() => {
-        // setPath(my.nickName);
-    });
+    const handleListItemClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        index: number
+    ) => {
+        setSelectedIndex(index);
+    };
 
     return (
         <PreviewPage click={props.props}>
@@ -98,8 +114,70 @@ export default function Preview(props: any) {
                 <PreviewRight>
                     <PreviewOpenSettingWrapper>
                         <PreviewOpenSettingTitle>
-                            공개 설정
+                            {isSave ? "저장위치 설정" : "공개 설정"}
                         </PreviewOpenSettingTitle>
+                        {isSave && (
+                            <Box
+                                sx={{
+                                    width: "100%",
+                                    height: 300,
+                                    maxWidth: 320,
+                                    bgcolor: "background.paper",
+                                    position: "fixed",
+                                    zIndex: 2,
+                                }}
+                            >
+                                <List
+                                    component="nav"
+                                    aria-label="main mailbox folders"
+                                >
+                                    <ListItemButton
+                                        selected={selectedIndex === 0}
+                                        onClick={(event) =>
+                                            handleListItemClick(event, 0)
+                                        }
+                                    >
+                                        <ListItemIcon>
+                                            <InboxIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary="Inbox" />
+                                    </ListItemButton>
+                                    <ListItemButton
+                                        selected={selectedIndex === 1}
+                                        onClick={(event) =>
+                                            handleListItemClick(event, 1)
+                                        }
+                                    >
+                                        <ListItemIcon>
+                                            <DraftsIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary="Drafts" />
+                                    </ListItemButton>
+                                </List>
+                                <Divider />
+                                <List
+                                    component="nav"
+                                    aria-label="secondary mailbox folder"
+                                >
+                                    <ListItemButton
+                                        selected={selectedIndex === 2}
+                                        onClick={(event) =>
+                                            handleListItemClick(event, 2)
+                                        }
+                                    >
+                                        <ListItemText primary="Trash" />
+                                    </ListItemButton>
+                                    <ListItemButton
+                                        selected={selectedIndex === 3}
+                                        onClick={(event) =>
+                                            handleListItemClick(event, 3)
+                                        }
+                                    >
+                                        <ListItemText primary="Spam" />
+                                    </ListItemButton>
+                                </List>
+                            </Box>
+                        )}
                         <PreviewOpenSettingButtonWrapper>
                             <Button
                                 onClick={(e) => clickOpen(e, true)}
@@ -201,6 +279,7 @@ export default function Preview(props: any) {
                     <PreviewSavePositionWrapper>
                         <PreviewSaveTitle>저장위치 설정</PreviewSaveTitle>
                         <Button
+                            onClick={clickSave}
                             sx={{
                                 height: "50px",
                                 fontSize: "1.35rem",
@@ -223,7 +302,7 @@ export default function Preview(props: any) {
                                 props.setClick(false);
                             }}
                         >
-                            나가기
+                            {isSave ? "취소" : "나가기"}
                         </WriteExitButton>
                         <WriteRegisterButton>출간하기</WriteRegisterButton>
                     </PreviewButtonWrapper>
